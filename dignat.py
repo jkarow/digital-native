a8a4608cc0112c2033c7d22fcbf7432eedf2892e
hotgluebanjo <66244111+hotgluebanjo@users.noreply.github.com>
2023-10-22 11:27:05 +0000

    Add averaging and problematic Sony cameras

--- dignat.py
+++ dignat.py
@@ -19,6 +19,7 @@ class Camera:
 # Sensor native to target matrices.
 # Currently problematic:
 # - Sony A7SIII
+# - Sony A7C
 CAMERA_MATRICES = [
     Camera("ARRI Alexa", "Alexa Wide Gamut", np.array([[1.16568936, -0.18400079, 0.01831143], [-0.0524356, 1.01472615, 0.03770945], [0.0270692, -0.24782896, 1.22075976]])),
     Camera("Canon EOS R3", "ITU-R BT.709", np.array([[2.02, -1.15, 0.14], [-0.17, 1.52, -0.36], [0.05, -0.55, 1.5]])),
@@ -31,7 +32,9 @@ CAMERA_MATRICES = [
     Camera("Panasonic Lumix DC-S1R", "ITU-R BT.709", np.array([[1.61, -0.52, -0.09], [-0.08, 1.15, -0.07], [ 0.05, -0.4,  1.35]])),
     Camera("Pentax 645Z", "ITU-R BT.709", np.array([[1.87, -0.81, -0.06], [-0.16, 1.56, -0.4], [0.09, -0.59, 1.5]])),
     Camera("Sony A7", "ITU-R BT.709", np.array([[1.93, -0.79, -0.14], [-0.16, 1.59, -0.43], [0.03, -0.44, 1.41]])),
+    Camera("Sony A7C", "ITU-R BT.709", np.array([[1.9, -0.73, -0.17], [-0.17, 1.46, -0.29], [0.13, -0.8, 1.67]])),
     Camera("Sony A7R V", "ITU-R BT.709", np.array([[1.95, -0.87, -0.08], [-0.15, 1.58, -0.43], [0.03, -0.45, 1.41]])),
+    Camera("Sony A7SIII", "ITU-R BT.709", np.array([[1.9, -0.71, -0.19], [-0.17, 1.47, -0.3], [0.13, -0.85, 1.72]])),
 ]
 
 def space_from_camera_matrix(cam):
@@ -56,6 +59,16 @@ def space_from_farthest_primaries(spaces, whitepoint, name):
                 dists[i] = dist
     return colour.RGB_Colourspace(name, primaries, whitepoint)
 
+def space_from_average_primaries(spaces, whitepoint, name):
+    sum = np.zeros((3, 2))
+
+    for space in spaces:
+        sum += space.primaries
+
+    primaries = sum / len(spaces)
+
+    return colour.RGB_Colourspace(name, primaries, whitepoint)
+
 def format_matrix_plaintext(mat, precision = 8):
     mat = np.round(mat, precision)
     return f"""\
@@ -68,7 +81,7 @@ def main():
     for camera in CAMERA_MATRICES:
         camera_spaces.append(space_from_camera_matrix(camera))
 
-    dignat = space_from_farthest_primaries(camera_spaces, DIGNAT_WHITEPOINT, "Digital Native")
+    dignat = space_from_average_primaries(camera_spaces, DIGNAT_WHITEPOINT, "Digital Native")
     dignat.use_derived_transformation_matrices()
 
     print("Digital Native to XYZ:")
